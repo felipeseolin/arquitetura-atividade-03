e6ffeb15617cf9b7036321e96e4194ecf7b22661
Felipe Seolin Bento <felipe.seolin@hotmail.com>
2019-10-23 19:43:52 -0300

    Gerenciamento de Paises

---
+++ app/src/main/java/com/exercicio/grupo/app/apresentacao/model/PaisModel.java
@@ -0,0 +1,16 @@
+package com.exercicio.grupo.app.apresentacao.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PaisModel {
+
+    private Long id;
+    private String nome;
+}
